99f761ddb2305b7336abd25cca4c6b12daf41607
Tristan Muller <Tristan_e_Muller@icloud.com>
2024-03-11 21:52:14 -0700

    Added UI scripts

---
+++ our project #1/Assets/Scripts/UI/ProgressToNewBiomeUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ProgressToNewBiomeUI : MonoBehaviour
+{
+    public TMP_Text distance;
+    public Image compassIMage;
+    float questDistance;
+    float questDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
