c14f1893eee15b11cfb88d0d6a9e41ff8e01b3c9
Tristan Muller <Tristan_e_Muller@icloud.com>
2024-04-08 16:14:06 -0700

    Finished Zooming and look Ahead

--- our project #1/Assets/Scripts/Camera/CameraFollow.cs
+++ our project #1/Assets/Scripts/Camera/CameraFollow.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
@@ -9,6 +10,7 @@ public class CameraFollow : MonoBehaviour
     [Range(0,1)]
     public float smoothSpeed = .5f;
     public float MinSmoothSpeed = .1f;
+    [Space(15)]
 
     public GameObject zoomCameraObj;
     public float zoomOutDist;
@@ -16,7 +18,11 @@ public class CameraFollow : MonoBehaviour
     float zoom;
     float zoomIn;
     float zoomOut;
-
+    [Space(15)]
+    [Tooltip("A bias to control camera's look ahead: 0 = target position and  1 = mouse position")]
+    [Range(0, 1)] public float lookAheadBias = .5f;
+    [Tooltip("A clamp to control how far the camera can look ahead, use  <= 0 for no clamp ")]
+    public float lookAheadClampingDist;
     public Vector3 offset;
 
     Vector3 smoothedPos;
@@ -26,12 +32,22 @@ public class CameraFollow : MonoBehaviour
     {
         
         float zoomLerp = MovementControl.player.rb.velocity.sqrMagnitude / Mathf.Pow(MovementControl.player.MaxSpeed, 2);
-        
+
+
+        Vector3 lookAhead = Vector3.Lerp(target.position, MovementControl.player.worldPosition, lookAheadBias);
+        lookAhead.y = target.position.y;
+
+        if (lookAheadClampingDist > 0 && Vector3.Distance(target.position, lookAhead) > lookAheadClampingDist)
+        {
+                Vector3 direction = (lookAhead - target.position).normalized;
+                lookAhead = target.position + direction * lookAheadClampingDist;
+        }
+
 
         zoom = Mathf.Lerp(-zoomOutDist, zoomInDist, zoomLerp);
 
 
-        Vector3 disieredPos = target.position + offset;
+        Vector3 disieredPos = lookAhead + offset;
         disieredPos.y = 0;
         smoothedPos = Vector3.SmoothDamp(transform.position, disieredPos, ref velocity, smoothSpeed, Mathf.Infinity, Time.deltaTime);
         transform.position = smoothedPos;
