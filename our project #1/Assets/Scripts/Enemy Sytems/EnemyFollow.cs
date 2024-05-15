23f5dad7671e6ea0e142517bdc3afe965be951a3
Tristan Muller <Tristan_e_Muller@icloud.com>
2024-05-14 18:02:29 -0700

    Finished converting Enemy Follow and projectile shooting

--- our project #1/Assets/Scripts/Enemy Sytems/EnemyFollow.cs
+++ our project #1/Assets/Scripts/Enemy Sytems/EnemyFollow.cs
@@ -3,6 +3,7 @@ using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 //[RequireComponent(typeof(Enemy))]
 public class EnemyFollow : MonoBehaviour, IEnemyBehaviorNode
@@ -13,6 +14,7 @@ public class EnemyFollow : MonoBehaviour, IEnemyBehaviorNode
     public EnemyPamater<float> speed;
     public EnemyPamater<float> targetDisFormTarget;
     public EnemyPamater<float> lookAngle;
+    public Vector3 rotationOffset;
 
     public BehaviorExitReturn behaviorExitReturn 
     {
@@ -32,7 +34,14 @@ public class EnemyFollow : MonoBehaviour, IEnemyBehaviorNode
     }
     [SerializeField] bool exitNode;
 
+    public bool drawDebugGizmos = false;
+
     public void OnEnterBehavior()
+    {
+        target = GameObject.FindGameObjectWithTag(targetTag).GetComponent<Transform>();
+    }
+
+    private void OnValidate()
     {
         target = GameObject.FindGameObjectWithTag(targetTag).GetComponent<Transform>();
     }
@@ -41,14 +50,19 @@ public class EnemyFollow : MonoBehaviour, IEnemyBehaviorNode
     {
         Vector3 targetPos = new Vector3(target.position.x, transform.position.y, target.position.z);
 
-        if ((transform.position - targetPos).sqrMagnitude > targetDisFormTarget.value)
+        if ((transform.position - targetPos).sqrMagnitude > Mathf.Pow(targetDisFormTarget.value, 2))
         {
             transform.position = Vector3.MoveTowards(transform.position, targetPos, speed.value * Time.deltaTime);
         }
-        else if ((transform.position - targetPos).sqrMagnitude < targetDisFormTarget.value)
+        else if ((transform.position - targetPos).sqrMagnitude < Mathf.Pow(targetDisFormTarget.value, 2))
         {
             transform.position = Vector3.MoveTowards(transform.position, targetPos, -speed.value * Time.deltaTime);
         }
+        Vector3 diference =  transform.position - target.position;
+
+        float angle = Mathf.Atan2(-diference.x, -diference.z) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(rotationOffset.x, angle + rotationOffset.y, rotationOffset.z);
 
         /*Vector3 difernce = target.position - transform.position;
         float angle = Mathf.Atan2(difernce.z, difernce.x) * Mathf.Rad2Deg;
@@ -56,5 +70,31 @@ public class EnemyFollow : MonoBehaviour, IEnemyBehaviorNode
         {
             transform.rotation = Quaternion.Euler(90, angle, 0);
         }*/
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(drawDebugGizmos) 
+        {
+            Vector3 diference = transform.position - target.position;
+        
+            Gizmos.color = Color.green;
+            Gizmos.DrawRay(transform.position, diference);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(transform.position, new Vector3(diference.x, 0, 0));
+        
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawRay(transform.position, new Vector3(0, diference.y, 0));
+        
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(transform.position, new Vector3(0, 0, diference.z));
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, .9f, .9f, .8f);
+        Gizmos.DrawWireSphere(target.position, targetDisFormTarget.value);
     }
 }
