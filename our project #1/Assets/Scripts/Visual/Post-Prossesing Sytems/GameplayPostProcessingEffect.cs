3724597f37a10b00dca7b97b151d703adcb95192
Tristan Muller <Tristan_e_Muller@icloud.com>
2024-06-22 19:40:49 -0700

    Outlined Framework for New Post Processing System

--- our project #1/Assets/Scripts/Visual/Post-Prossesing Sytems/GameplayPostProcessingEffect.cs
+++ our project #1/Assets/Scripts/Visual/Post-Prossesing Sytems/GameplayPostProcessingEffect.cs
@@ -1,18 +1,41 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Events;
+using System;
 
 public class GameplayPostProcessingEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [ReadOnly] public Volume volume;
+    public GameplayEffect effect;
 
-    // Update is called once per frame
-    void Update()
+    public void PlayEffect()
     {
-        
+        StartCoroutine(RunEffect());
+    }
+    public IEnumerator RunEffect()
+    {
+        //setup effect by adding all overides
+        //call all on start
+        //while elapsed <= duration call all on update
+        //else call all on end and end by removing overides
+
+        throw new NotImplementedException("To do:");
+    }
+}
+
+public class GameplayEffect
+{
+    [InlineEditor]
+    public List<VolumeComponent> componants;
+    public List<IEffectControl> OnStart;
+    public List<IEffectControl> OnUpdate;
+    public List<IEffectControl> OnEnd;
+    public interface IEffectControl
+    {
+        // the interface used for controlling gameplay effects, allowing for run to puase exicution of onUpdate until its finished 
+        public IEnumerator Invoke();
     }
 }
