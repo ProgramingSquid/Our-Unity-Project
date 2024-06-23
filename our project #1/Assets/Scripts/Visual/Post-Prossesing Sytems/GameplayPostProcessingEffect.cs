d5b3effef5917f9b53a5dfa61d59ee7257f064b9
Tristan Muller <Tristan_e_Muller@icloud.com>
2024-06-23 09:27:53 -0700

    Started to write the implementation of on update, on start, and. on end
    Still need to find a way to be able to set overrides from the inspector.

--- our project #1/Assets/Scripts/Visual/Post-Prossesing Sytems/GameplayPostProcessingEffect.cs
+++ our project #1/Assets/Scripts/Visual/Post-Prossesing Sytems/GameplayPostProcessingEffect.cs
@@ -11,31 +11,81 @@ public class GameplayPostProcessingEffect : MonoBehaviour
     [ReadOnly] public Volume volume;
     public GameplayEffect effect;
 
+    [Button()]
     public void PlayEffect()
     {
         StartCoroutine(RunEffect());
     }
     public IEnumerator RunEffect()
     {
+        float elapsed = 0;
         //setup effect by adding all overides
+        foreach (var effectComponant in effect.componants)
+        {
+            volume.profile.Add(effectComponant.GetType());
+        }
         //call all on start
+        foreach (var effectControl in effect.OnStart)
+        {
+            effectControl.Invoke();
+        }
         //while elapsed <= duration call all on update
+        while (elapsed< effect.maxDuration)
+        {
+            foreach (var effectControl in effect.OnUpdate)
+            {
+                effectControl.Invoke();
+            }
+            yield return null;
+        }
         //else call all on end and end by removing overides
+        foreach (var effectControl in effect.OnEnd)
+        {
+            effectControl.Invoke();
+        }
 
-        throw new NotImplementedException("To do:");
+        foreach (var componant in volume.profile.components)
+        {
+            volume.profile.components.Remove(componant);
+        }
     }
 }
-
+[Serializable]
 public class GameplayEffect
 {
-    [InlineEditor]
-    public List<VolumeComponent> componants;
-    public List<IEffectControl> OnStart;
-    public List<IEffectControl> OnUpdate;
-    public List<IEffectControl> OnEnd;
+    public float maxDuration;
+    [SerializeReference]
+    [InlineEditor] public List<VolumeComponent> componants = new List<VolumeComponent>();
+    [InlineEditor] public List<IEffectControl> OnStart;
+    [InlineEditor] public List<IEffectControl> OnUpdate;
+    [InlineEditor] public List<IEffectControl> OnEnd;
     public interface IEffectControl
     {
         // the interface used for controlling gameplay effects, allowing for run to puase exicution of onUpdate until its finished 
         public IEnumerator Invoke();
+    }
+
+    public class Wait : IEffectControl
+    {
+        public float seconds;
+        public IEnumerator Invoke()
+        {
+            yield return new WaitForSeconds(seconds);
+        }
+    }
+
+    public class SetFloatValue : IEffectControl
+    {
+        public VolumeComponent component;
+        [SerializeReference] public VolumeParameter volumeParameter;
+        public float value;
+        public IEnumerator Invoke()
+        {
+            if(volumeParameter is MinFloatParameter)
+            {
+                (volumeParameter as MinFloatParameter).value = value;
+            }
+            yield return null;
+        }
     }
 }
