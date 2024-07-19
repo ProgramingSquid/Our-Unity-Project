a915e77f15ec3b14bfe02306bac0ccb0e5acf1cc
Tristan Muller <Tristan_e_Muller@icloud.com>
2024-07-19 14:02:13 -0700

    Started adding support for new generation base biome blending solution

--- our project #1/Assets/Scripts/Map Generation/MapManager.cs
+++ our project #1/Assets/Scripts/Map Generation/MapManager.cs
@@ -10,7 +10,8 @@ using UnityEngine;
 public static class MapManager
 {
     public static List<Biome> biomes = new List<Biome>();
-    
+    [SerializeReference]
+    public static GenerationBase.OffsetCalculation baseTerrainCalculation;
 
     public static void GenerateMap()
     {
@@ -47,7 +48,6 @@ public static class MapManager
 [CreateAssetMenu(fileName = "New Biome", menuName = "New Biome")]
 public class Biome : ScriptableObject
 {
-
     [Serializable]
     public struct GenerationFeatureProperties
     {
@@ -74,4 +74,30 @@ public class Biome : ScriptableObject
 public abstract class GenerationFeature
 {
     public abstract void Generate();
+}
+
+public abstract class GenerationBase
+{
+    public OffsetCalculation offset;
+    public abstract void Generate(Vector3 GameObjectPos);
+    /// <summary>
+    /// An optional method which can be overridden for additional control over how Generated Chunk GameObjects' 
+    /// positions are generated relative to a grid position.
+    /// </summary>
+    /// <param name="gridPos">
+    /// A position vector representing the position of the chunk cell in grid cardiants </param>
+    /// <returns>A Vector3 representing where the GameObject's position should be </returns>
+    public virtual Vector3 CalculatePosition(Vector2Int gridPos)
+    {
+        return (Vector2)gridPos;
+    }
+
+    /// <summary>
+    /// A class used to define how vertex offsets on a base mesh should be calculated, allowing for control over biome 
+    /// blending base terrain generation.
+    /// </summary>
+    public abstract class OffsetCalculation
+    {
+        public abstract Vector3 Calculate(Vector3 pos);
+    }
 }
